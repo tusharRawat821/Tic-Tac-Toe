023ac7102b5fb97ec7335e2d1f62d22718ea6775
Tushar Rawat <tusharrawat821@gmail.com>
2021-10-13 14:28:13 +0530

    Rename runner class.

--- GameRunner.java
+++ GameRunner.java
@@ -2,14 +2,13 @@
  * This is the runner class (main class from where execution of Tic-Tac-Toe game starts)
  * 
  */
-public class OXGame {
+public class GameRunner {
     public static void main(String[] args) {
         System.out.println("Game Starts...\n\n");
-        
+
         TicTacToe.play();
 
         System.out.println("\n\nGame Ends.....");
-
     }
 
 }
